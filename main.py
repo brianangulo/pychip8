e85bf02877f4f7652a2ffa0358cb4fb600f5d36f
Brian Angulo <bangulo219@gmail.com>
2022-07-31 15:24:13 -0500

    update: main todo

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from tkinter import Canvas, Tk
-
-def main():
-    print('hello world')
-
-master = Tk()
-canvas = Canvas(master, bg="black", height=32, width=64)
-
-def hello(event):
-    print(event.char)
-
-master.bind('<Key s>', hello)
-# canvas.bind('<Key>', hello)
-canvas.pack()
-canvas.mainloop()
+#TODO: run emulator from this file
