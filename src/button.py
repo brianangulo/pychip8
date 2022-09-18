c785cd9d15856af6be80a0891cd9b272a7bcafe7
Brian Angulo <bangulo219@gmail.com>
2022-09-18 11:57:36 -0500

    update: button component highlight on hover and on click event

--- src/button.py
+++ src/button.py
@@ -1,18 +1,35 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pygame
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+
 class Button:
-    def __init__(self, color, x, y, width, height, pygame, text=''):
+    def __init__(self, color, x, y, width, height, pygame_instance: pygame, text=''):
+        self.default_color = color
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
-        self.pygame = pygame
+        self.pygame = pygame_instance
 
     def draw(self, win, outline=None):
         # Call this method to draw the button on the screen
+        self.mouseover_update_color()
+        self.on_click()
         if outline:
             self.pygame.draw.rect(
-                win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+                win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
 
         self.pygame.draw.rect(
             win, self.color, (self.x, self.y, self.width, self.height), 0)
@@ -21,13 +38,25 @@ class Button:
             default_font = self.pygame.font.get_default_font()
             font = self.pygame.font.SysFont(default_font, 30)
             text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2),
-                     self.y + (self.height/2 - text.get_height()/2)))
+            win.blit(text, (self.x + (self.width / 2 - text.get_width() / 2),
+                            self.y + (self.height / 2 - text.get_height() / 2)))
 
-    def isOver(self, pos):
+    def mouseover_update_color(self):
+        # updates the color as needed to act change effect when mouse over
+        if self.is_over(self.pygame.mouse.get_pos()):
+            self.color = RED
+        elif not self.is_over(self.pygame.mouse.get_pos()) and self.color == RED:
+            self.color = self.default_color
+
+    def on_click(self):
+        mouse_buttons = self.pygame.mouse.get_pressed()
+        if mouse_buttons[0] and self.is_over(self.pygame.mouse.get_pos()):
+            return True
+
+    def is_over(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
                 return True
 
         return False
